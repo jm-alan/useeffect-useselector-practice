72fc2c981e6be3954440b75f246d8fe9f08031db
Joshua <j.maxwell.alan@gmail.com>
2021-07-30 18:03:06 -0400

    .

--- frontend/src/components/Posts/index.js
+++ frontend/src/components/Posts/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 
 import { CreatePost, GetPosts, UnloadPosts } from '../../store/posts';
 import SinglePost from './SinglePost';
